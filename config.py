7ea32690f508b95dc0678ba7d773bec59cdbd27e
Steve Tornio <swtornio@gmail.com>
2025-07-06 14:24:18 -0500

    tidying up the config.

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 # config.py
 import os
 
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-# DB_PATH = os.path.join(DATA_DIR, "games.db")
-
-# config.py
-
 DB_PATH = "data/games.db"
 TABLE_NAME = "games"
